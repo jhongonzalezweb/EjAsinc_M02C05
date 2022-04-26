5426769454e2d40698404d9501951a6a44070460
Jhon Gonzalez <66422237+jhongonzalezweb@users.noreply.github.com>
2022-04-26 17:17:30 -0400

    Update ejercicio.js

--- ejercicio.js
+++ ejercicio.js
@@ -1,10 +1,9 @@
-
 function sumArray(num) {
 
     myArreglo = num
-    
+
     var total = 0;
-    
+
     myArreglo.forEach(e => {
         total = parseInt(total) + parseInt(e);
     });
@@ -13,9 +12,21 @@ function sumArray(num) {
     return total;
 }
 
-var res = sumArray([1,2,3]);
+var n1 = prompt("Dime el numero uno: ")
+var n2 = prompt("Dime el numero dos: ")
+var n3 = prompt("Dime el numero tres: ")
+var res = sumArray([n1, n2, n3]);
+
 console.log(res);
-var res = sumArray([10,3,10,4]);
-console.log(res);
-var res = sumArray([-5,100]);
-console.log(res);
+
+// var res = sumArray([10,3,10,4]);
+// console.log(res);
+// var res = sumArray([-5,100]);
+// console.log(res);
+
+
+function revelarRes() {
+
+    document.getElementById("demo").innerHTML = res;
+
+}
